60e5b93b9be975b28d148386cff925477302b30b
milkygalicia9 <202080177@psu.palawan.edu.ph>
2023-12-01 02:31:29 +0800

    done changes

---
+++ project/cardquest/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from .models import PokemonCard
+
+admin.site.register(PokemonCard)
+
+@admin.register(PokemonCard)
+class PokemonAdmin(admin.ModelAdmin) :
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
